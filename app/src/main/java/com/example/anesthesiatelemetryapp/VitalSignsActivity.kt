18d0f9ceac1b4ed9f067550b6b72ae08ff9a56e4
Tim <tjcaole4@gmail.com>
2024-02-03 12:00:39 -0800

    Login page has been desined and coded

---
+++ app/src/main/java/com/example/anesthesiatelemetryapp/VitalSignsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.anesthesiatelemetryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VitalSignsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vital_signs)
+    }
+}
